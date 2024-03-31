e83b208617a3cc90fb789c7a7110ac330bed867a
Alex Oasian <opendev.top@gmail.com>
2024-04-01 01:04:19 +0800

    Update SimpleStorge

--- Lesson2/SimpleStorge.sol
+++ Lesson2/SimpleStorge.sol
@@ -2,9 +2,11 @@
 pragma solidity ^0.8.25;
 
 contract SimpleStorge {
-    uint256 my_variable;
+    uint256 public my_variable;
 
     function store(uint256 _variable) public {
         my_variable = _variable;
     }
 }
+
+// 0xDA0bab807633f07f013f94DD0E6A4F96F8742B53
