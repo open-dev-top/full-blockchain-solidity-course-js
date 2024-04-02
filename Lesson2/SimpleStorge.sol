e42d39ef0f06f4b301e144123fb355a8ac4aeb38
open-dev-top <162958903+open-dev-top@users.noreply.github.com>
2024-04-02 23:59:03 +0800

    Update SimpleStorge.sol

--- Lesson2/SimpleStorge.sol
+++ Lesson2/SimpleStorge.sol
@@ -3,10 +3,28 @@ pragma solidity ^0.8.25;
 
 contract SimpleStorge {
     uint256 public my_variable;
+    
+    People[] public people;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    mapping (string => uint256) public nameToFavoriteNumber;
+
 
     function store(uint256 _variable) public {
         my_variable = _variable;
     }
-}
 
-// 0xDA0bab807633f07f013f94DD0E6A4F96F8742B53
+
+    function retrieve() public view returns(uint256) {
+        return my_variable;
+    }
+
+    function addPeople(uint256 _favoriteNumber, string memory _name) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
