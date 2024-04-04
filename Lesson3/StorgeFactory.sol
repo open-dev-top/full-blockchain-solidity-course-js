4930c6106491ada6d5819df55d0792ea233d3e22
open dev <opendev.top@gmail.com>
2024-04-04 12:07:21 +0800

    Update SimpleStorage.sol

--- Lesson3/StorgeFactory.sol
+++ Lesson3/StorgeFactory.sol
@@ -13,14 +13,10 @@ contract StorgeFactory {
     }
 
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
-        // 存储 simpleStorage contract 的地址，自动封装 ABI
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        simpleStorage.store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
-        // 根据地址定位合同
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
-        return simpleStorage.retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
