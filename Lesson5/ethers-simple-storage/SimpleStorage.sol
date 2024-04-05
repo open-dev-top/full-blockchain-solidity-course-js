b11b25aef574f949bb544e84acce3b5ad14c9a1c
open dev <opendev.top@gmail.com>
2024-04-05 21:53:46 +0800

    yarn add 0.8.7-fixed

--- Lesson5/ethers-simple-storage/SimpleStorage.sol
+++ Lesson5/ethers-simple-storage/SimpleStorage.sol
@@ -1,6 +1,10 @@
+// I'm a comment!
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.25;
+pragma solidity 0.8.7;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
     uint256 favoriteNumber;
