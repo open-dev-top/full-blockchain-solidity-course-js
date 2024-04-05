a2afdfb49a9de63062e2c13300aa14e0e622a4cb
open dev <opendev.top@gmail.com>
2024-04-05 14:05:56 +0800

    Add deploy.js

---
+++ Lesson5/ethers-simple-storage/deploy.js
@@ -0,0 +1,15 @@
+// synchronous [solidity]
+// asynchronous [javascript]
+
+async function main() {
+  console.log("hi");
+  let variable = 5;
+  console.log(variable);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
