84439aae7399e6039da17d36e4f087343fe66871
open dev <opendev.top@gmail.com>
2024-04-06 21:48:41 +0800

    Finshed test-deploy.js

--- Lesson6/hardhat-simple-storage/test/test-deploy.js
+++ Lesson6/hardhat-simple-storage/test/test-deploy.js
@@ -1,33 +1,63 @@
-const {ethers} = require("hardhat")
-const {expect, assert} = require("chai")
+const { ethers } = require("hardhat")
+const { expect, assert } = require("chai")
 
-describe("SimpleStorage", function(){
-  let SimpleStorageFactory, simpleStorage
-  beforeEach(async function(){
-    SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
-    simpleStorage = await SimpleStorageFactory.deploy()
-  })
-  it.only("Should start with favorite number of 0", async function(){
-    const currentValue = await simpleStorage.retrieve()
-    const expectedValue = "0"
+describe("SimpleStorage", function () {
+    let SimpleStorageFactory, simpleStorage
+    beforeEach(async function () {
+        SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+        simpleStorage = await SimpleStorageFactory.deploy()
+    })
+    it.only("Should start with favorite number of 0", async function () {
+        const currentValue = await simpleStorage.retrieve()
+        const expectedValue = "0"
 
-    assert.equal(currentValue.toString(), expectedValue)
-  })
-  it.only("Should update when we call store", async function(){
-    const expectedValue = "7"
-    const transactionResponse = await simpleStorage.store(expectedValue)
-    await transactionResponse.wait(1)
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+    it.only("Should update when we call store", async function () {
+        const expectedValue = "7"
+        const transactionResponse = await simpleStorage.store(expectedValue)
+        await transactionResponse.wait(1)
 
-    const currentValue = await simpleStorage.retrieve()
-    assert.equal(currentValue.toString(), expectedValue)
-  })
-  it.only("Should add people with favornumber when we call add Person", async function(){
-    const expectedPeople = "Lisa"
-    const expectedValue = "2"
-    const transactionResponse = await simpleStorage.addPerson(expectedPeople, expectedValue)
-    await transactionResponse.wait(1)
+        const currentValue = await simpleStorage.retrieve()
+        assert.equal(currentValue.toString(), expectedValue)
+    })
 
-    const currentValue = await simpleStorage.retrieve()
-    assert.equal(currentValue.toString(), expectedValue)
-  })
+    const { ethers } = require("hardhat")
+    const { expect, assert } = require("chai")
+
+    describe("SimpleStorage", function () {
+        let SimpleStorageFactory, simpleStorage
+        beforeEach(async function () {
+            SimpleStorageFactory =
+                await ethers.getContractFactory("SimpleStorage")
+            simpleStorage = await SimpleStorageFactory.deploy()
+        })
+        it.only("Should start with favorite number of 0", async function () {
+            const currentValue = await simpleStorage.retrieve()
+            const expectedValue = "0"
+
+            assert.equal(currentValue.toString(), expectedValue)
+        })
+        it.only("Should update when we call store", async function () {
+            const expectedValue = "7"
+            const transactionResponse = await simpleStorage.store(expectedValue)
+            await transactionResponse.wait(1)
+
+            const currentValue = await simpleStorage.retrieve()
+            assert.equal(currentValue.toString(), expectedValue)
+        })
+        it.only("Should work correctly with the people struct and array", async function () {
+            const expectedPersonName = "Patrick"
+            const expectedFavoriteNumber = "16"
+            const transactionResponse = await simpleStorage.addPerson(
+                expectedPersonName,
+                expectedFavoriteNumber,
+            )
+            await transactionResponse.wait(1)
+            const { favoriteNumber, name } = await simpleStorage.people(0)
+
+            assert.equal(name, expectedPersonName)
+            assert.equal(favoriteNumber, expectedFavoriteNumber)
+        })
+    })
 })
