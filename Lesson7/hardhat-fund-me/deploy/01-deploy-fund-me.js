3ffcfa1d19a994631b43367d1885367a4eefa5bb
open dev <opendev.top@gmail.com>
2024-04-10 15:25:47 +0800

    Deploy by Remix

--- Lesson7/hardhat-fund-me/deploy/01-deploy-fund-me.js
+++ Lesson7/hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -1,11 +1,22 @@
 // function deployFunc() {
 //     console.log("Hi")
+//      hre.getNamedAccounts()
+//      hre.deployments
 // }
+
+const { network } = require("hardhat")
 
 // module.exports.default = deployFunc
 
-module.exports = async (hre) => {
-    const { getNamedAccounts, deployments } = hre
-    console.log(getNamedAccounts)
-    console.log(deployments)
+// module.exports = async (hre) => {
+//     const { getNamedAccounts, deployments } = hre
+//     console.log(getNamedAccounts)
+//     console.log(deployments)
+// }
+
+// Get getNamedAccounts and deployments from hre
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
 }
---
+++ Lesson7/hardhat-fund-me/helper-hardhat-config.js
@@ -0,0 +1,14 @@
+const networkConfig = {
+    11155111: {
+        name: "sepolia",
+        ethUsdPriceFeed: "0x694AA1769357215DE4FAC081bf1f309aDC325306",
+    },
+    137: {
+        name: "polygon",
+        ethUsdPriceFeed: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
+    },
+}
+
+module.exports = {
+    networkConfig,
+}
