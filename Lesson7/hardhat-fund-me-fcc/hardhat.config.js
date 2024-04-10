83e84c06a2137718cd567cdeed68dc871147d75d
open dev <opendev.top@gmail.com>
2024-04-10 21:43:50 +0800

    Fixed web3_learn1_part11

--- Lesson7/hardhat-fund-me-fcc/hardhat.config.js
+++ Lesson7/hardhat-fund-me-fcc/hardhat.config.js
@@ -1,10 +1,6 @@
-const { version } = require("chai")
-
-// require("@nomicfoundation/hardhat-toolbox")
 require("@nomiclabs/hardhat-waffle")
 require("hardhat-gas-reporter")
 require("@nomiclabs/hardhat-etherscan")
-// require("@nomicfoundation/hardhat-verify")
 require("dotenv").config()
 require("solidity-coverage")
 require("hardhat-deploy")
@@ -40,11 +36,11 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.8",
+                version: "0.8.7",
             },
             {
-                version: "0.6.6"
-            }
+                version: "0.6.6",
+            },
         ],
     },
     etherscan: {
@@ -61,10 +57,7 @@ module.exports = {
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
-            // 1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-        },
-        user: {
-            default: 1,
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
     },
     mocha: {
