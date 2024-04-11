2b82544e3bf0f0144c31a09ce9d69904f3713f01
open dev <opendev.top@gmail.com>
2024-04-11 16:32:17 +0800

    Add FundMe.test.js

--- Lesson7/hardhat-fund-me/contracts/FundMe.sol
+++ Lesson7/hardhat-fund-me/contracts/FundMe.sol
@@ -1,89 +1,105 @@
 // SPDX-License-Identifier: MIT
+// Pragma
 pragma solidity ^0.8.8;
-
+// Imports
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
+import "hardhat/console.sol";
+// Error Codes
+error FundMe_NotOwner();
 
-error NotOwner();
-
+// Interface, Libraries, Contracts
+/**
+ * @title 1
+ * @author 2
+ * @notice 3
+ * @dev 4
+ */
 contract FundMe {
+    // Type Declatations
     using PriceConverter for uint256;
 
-    mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
+    // State Variables
+    mapping(address => uint256) public s_addressToAmountFunded;
+    address[] public s_funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
+    address public immutable i_owner;
     uint256 public constant MINIMUM_USD = 10 * 10 ** 18;
-    
-    AggregatorV3Interface public priceFeed;
+    AggregatorV3Interface public s_priceFeed;
 
-    constructor(address priceFeedAddress) {
-        i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
-    }
-
-    function fund() public payable {
-        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
-    }
-    
-    // function getVersion() public view returns (uint256){
-    //     // ETH/USD price feed address of Sepolia Network.
-    //     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-    //     return priceFeed.version();
-    // }
-    
-    modifier onlyOwner {
+    // Modifiers
+    modifier onlyOwner() {
         // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
+        if (msg.sender != i_owner) revert FundMe_NotOwner();
         _;
     }
-    
+
+    // Functions
+    constructor(address s_priceFeedAddress) {
+        console.log("The constructor has been called.");
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(s_priceFeedAddress);
+    }
+
+    /**
+     * @notice 1
+     * @dev 2
+     * param 3
+     * returns 4
+     */
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    // function getVersion() public view returns (uint256){
+    //     // ETH/USD price feed address of Sepolia Network.
+    //     AggregatorV3Interface s_priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    //     return s_priceFeed.version();
+    // }
+
     function withdraw() public onlyOwner {
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
         // // transfer
         // payable(msg.sender).transfer(address(this).balance);
         // // send
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess, "Send failed");
         // call
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \ 
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback() 
-    //     /   \ 
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
 
-    fallback() external payable {
-        fund();
+    function cheaperWithdraw() public payable onlyOwner {
+        address[] memory funders = s_funders;
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
     }
-
-    receive() external payable {
-        fund();
-    }
-
 }
-
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
--- Lesson7/hardhat-fund-me/hardhat.config.js
+++ Lesson7/hardhat-fund-me/hardhat.config.js
@@ -56,7 +56,8 @@ module.exports = {
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
-        // coinmarketcap: COINMARKETCAP_API_KEY,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
     },
     namedAccounts: {
         deployer: {
