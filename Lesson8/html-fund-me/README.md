bf18fbe4a8b04127574ce62253390d1b730cf02d
open dev <opendev.top@gmail.com>
2024-04-11 23:47:25 +0800

    Remove Lesson7/hardhat-fund-me-fcc

--- Lesson8/html-fund-me/README.md
+++ Lesson8/html-fund-me/README.md
@@ -2,4 +2,6 @@
 yarn add --dev http-server
 
 yarn http-server
+
+yarn add --dev prettier
 ```
--- Lesson8/html-fund-me/index.js
+++ Lesson8/html-fund-me/index.js
@@ -1,14 +1,40 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+// console.log(ethers)
+
 async function connect() {
-  if (typeof window.ethereum !== "undefined") {
-    console.log("Your metamask has been detected!");
-    await window.ethereum.request({
-      method: "eth_requestAccounts",
-      params: [],
-    });
-    console.log("Your metamask has been conected!");
-    document.getElementById("connectButton").innerHTML = "CONNECTED";
-  } else {
-    console.log("Please install a metamask.");
-    document.getElementById("connectButton").innerHTML = "ERROR";
-  }
+    if (typeof window.ethereum !== "undefined") {
+        console.log("Your metamask has been detected!")
+        await window.ethereum.request({
+            method: "eth_requestAccounts",
+            params: [],
+        })
+        console.log("Your metamask has been conected!")
+        connectButton.innerHTML = "CONNECTED"
+    } else {
+        console.log("Please install a metamask.")
+        connectButton.innerHTML = "ERROR"
+    }
 }
+
+// fund function
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        // provider: connection to the blockchain
+        // signer: wallet, some one with some gas
+        // contract that we are interacting with
+        // ^ ABI & Address
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const singer = provider.getSigner()
+        // console.log(singer)
+        const contract = ""
+    }
+}
+
+// withdraw function
