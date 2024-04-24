b90779ce1bfcf998e4b1dab9712dbc855bf71257
open dev <opendev.top@gmail.com>
2024-04-24 18:00:22 +0800

    Save an endpoint

--- Lesson10/nextjs-smartcontract-lottery/components/LotteryEntrance.js
+++ Lesson10/nextjs-smartcontract-lottery/components/LotteryEntrance.js
@@ -1,5 +1,14 @@
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useMoralis } from "react-moralis"
 
 export default function LotteryEntrance() {
+    const { chainId } = useMoralis()
+    console.log("chainId", chainId)
+
+    // const { runContractFunction: enterRaffle } = useWeb3Contract({
+    //     abi: abi,
+    //     contractAddress: contractAddresses,
+    // })
     return <div>Hi from LotteryEntrance component</div>
 }
