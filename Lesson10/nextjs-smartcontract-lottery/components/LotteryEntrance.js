38b22b05af720bb99d82f9e01d3f47e9ccd2b4cd
open dev <opendev.top@gmail.com>
2024-04-24 21:56:35 +0800

    Add NotificationProvider

--- Lesson10/nextjs-smartcontract-lottery/components/LotteryEntrance.js
+++ Lesson10/nextjs-smartcontract-lottery/components/LotteryEntrance.js
@@ -3,6 +3,7 @@ import { abi, contractAddresses } from "../constants"
 import { useMoralis } from "react-moralis"
 import { useEffect, useState } from "react"
 import { ethers } from "ethers"
+import { useNotification } from "web3uikit"
 
 export default function LotteryEntrance() {
     const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
@@ -10,6 +11,8 @@ export default function LotteryEntrance() {
     const chainId = parseInt(chainIdHex)
     const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
     const [entranceFee, setEntranceFee] = useState("0")
+
+    const dispatch = useNotification()
 
     const { runContractFunction: enterRaffle } = useWeb3Contract({
         abi: abi,
@@ -37,6 +40,21 @@ export default function LotteryEntrance() {
         }
     }, [isWeb3Enabled])
 
+    const handleSuccess = async function (tx) {
+        await tx.wait(1)
+        handleNewNotification(tx)
+    }
+
+    const handleNewNotification = function () {
+        dispatch({
+            type: "info",
+            message: "Transaction Complete!",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
     return (
         <div>
             Hi from lottery entrance!
@@ -44,7 +62,10 @@ export default function LotteryEntrance() {
                 <div>
                     <button
                         onClick={async function () {
-                            await enterRaffle()
+                            await enterRaffle({
+                                onSuccess: handleSuccess,
+                                onError: (error) => console.log(error),
+                            })
                         }}
                     >
                         Enter Raffle
