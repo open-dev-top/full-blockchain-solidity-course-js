2b4e72b5a9916898450c404e0490a487ff6efc38
open dev <opendev.top@gmail.com>
2024-04-25 22:38:32 +0800

    Finished borrow

--- Lesson13/hardhat-defi/scripts/aaveBorrow.js
+++ Lesson13/hardhat-defi/scripts/aaveBorrow.js
@@ -16,11 +16,20 @@ async function main() {
     await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
     console.log("Deposited!")
     let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(lendingPool, deployer)
-    //
+    // getPrice
     const daiPrice = await getDaiPrice()
     const amountDaiToBorrow = availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
-    console.log(`You can borrow ${amountDaiToBorrow} DAI`)
     const amountDaiToBorrowWei = ethers.utils.parseEther(amountDaiToBorrow.toString())
+    const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+    // borrow
+    await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer)
+    await getBorrowUserData(lendingPool, deployer)
+}
+
+async function borrowDai(daiAddress, lendingPool, amountDaiToBorrowWei, account) {
+    const borrowTx = await lendingPool.borrow(daiAddress, amountDaiToBorrowWei, 2, 0, account)
+    await borrowTx.wait(1)
+    console.log("You've borrowed!")
 }
 
 async function getDaiPrice() {
